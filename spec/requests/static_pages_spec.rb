703e2cb1c7264d636560e96fd3c01edf0db34f37
takashi@14.10 <kawamoto@tkl.iis.u-tokyo.ac.jp>
2014-06-02 19:19:38 -0700

    enshu 3.5.2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
     it "shoud have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -9,7 +12,7 @@ describe "StaticPages" do
     end
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
   describe "Help page" do
@@ -19,7 +22,7 @@ describe "StaticPages" do
     end
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title('Help')
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
   describe "About page" do
@@ -29,7 +32,7 @@ describe "StaticPages" do
     end
     it "should have the title 'About Us'" do
       visit'/static_pages/about'
-      expect(page).to have_title("About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
   describe "Contact page" do
@@ -39,7 +42,7 @@ describe "StaticPages" do
     end
     it "should have the title 'Contact'" do
       visit'/static_pages/contact'
-      expect(page).to have_title("Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
